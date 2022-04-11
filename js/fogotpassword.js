f04e548e5ac409d057ccc6c0f6f46f1131486b3d
doreen38 <milliamsedet23@gmail.com>
2022-04-11 15:12:18 +0100

    update

---
+++ js/fogotpassword.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("myform");
+let email= document.getElementById("floatingInput");
+
+
+
+
+form.add
---
+++ js/restpassword.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("myform");
+let password= document.getElementById("password");
+let confirmPassword= document.getElementById("confirmPassword");
+
+
+from.addEventListener('submit', function() {
+    console.log('form');
+});
