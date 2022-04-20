7c18adf7a6a91d55c1a709d0652b3f14ca3d9ba9
HANNIEL21 <dhanniel21@gmail.com>
2022-04-20 09:54:17 +0100

    displaying user name

--- chat.js
+++ chat.js
@@ -1,24 +1,39 @@
-const form = document.getElementById("form")
+let send = document.getElementById("sendbtn");
+let msgEl = document.getElementById("content");
 
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-
-    const content = document.getElementById("content");
-    const input = document.getElementById("typing");
-    const txt = input.value
-    const span = document.createElement("span");
-    const p = document.createElement("p")
-
-    if (input.value !== "") {
-        p.classList.add("text-light")
-        span.classList.add("bg-secondary", "rounder-pill", "p-3")
-
-        p.append(txt);
-        content.appendChild(p);
-    }
-
-    form.reset();
-    console.log(txt);
+var pubnub = new PubNub({
+    publishKey: "pub-c-17e22ec5-b9e3-4b38-900e-fb2f86d81b82",
+    subscribeKey: "sub-c-bb554d48-bbf5-11ec-b9a7-7ec486788b75",
+    uuid: "internsChatApp"
 })
+
+function sendMessage(txt) {
+
+    pubnub.publish({
+        channel: "interns",
+        message: {
+            description: txt,
+            sender: pubnub.getUUID()
+        }
+    })
+}
+pubnub.addListener({
+    message: function (msg) {
+        msgEl.innerHTML += `
+        <div class="message-content bg-info">
+            <div class="message-sender">${msg.message.sender}</div>
+                <p>${msg.message.description}</p>
+            </div>
+        `;
+    },
+})
+pubnub.subscribe({
+    channels: ["interns"]
+});
+
+
+send.addEventListener("click", e => {
+    console.log(pubnub);
+    sendMessage(document.getElementById("typing").value);
+    document.getElementById("typing").value = "";
+});
